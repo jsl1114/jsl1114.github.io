67269351a0877b0b0cb9a23c72958573389d24b4
jsl1114 <ljs11141118@gmail.com>
2025-02-14 15:59:15 -0500

    extra padding for mobile view

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -5,7 +5,7 @@ const Education = () => {
   return (
     <div className='border-b border-neutral-900 pb-4'>
       <h1 className='my-20 text-4xl text-center'>Education</h1>
-      <div>
+      <div className='px-2'>
         {EDUCATION.map((edu, i) => (
           <EducationCard
             school={edu.school}

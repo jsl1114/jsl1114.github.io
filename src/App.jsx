3bf3ac6ee287fce742d1d717edd8903eb713f203
jsl1114 <ljs11141118@gmail.com>
2025-02-28 15:32:57 -0500

    optimization: safari hide toolbar on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ import Footer from './components/Footer'
 function App() {
   return (
     <>
-      <div className='absolute h-full w-full overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 top-0 z-[-2] bg-neutral-950 bg-[radial-gradient(ellipse_60%_60%_at_50%_-10%,rgba(87,6,140,0.5),rgba(0,0,0,0))]'>
+      <div className='absolute min-h-full w-full overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 top-0 z-[-2] bg-neutral-950 bg-[radial-gradient(ellipse_60%_60%_at_50%_-10%,rgba(87,6,140,0.5),rgba(0,0,0,0))]'>
         <Navbar />
-        <div className='container mx-auto px-8'>
+        <div className='container mx-auto px-8 mt-32'>
           <Hero />
           <Technologies />
           <Education />

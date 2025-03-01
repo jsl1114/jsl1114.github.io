8e756351e80d69d80869daf17421acc585ba1ded
jsl1114 <ljs11141118@gmail.com>
2025-02-28 21:06:31 -0500

    update: mt and animaiton

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <div className='absolute min-h-full w-full overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 top-0 z-[-2] bg-neutral-950 bg-[radial-gradient(ellipse_60%_60%_at_50%_-10%,rgba(87,6,140,0.5),rgba(0,0,0,0))]'>
         <Navbar />
-        <div className='container mx-auto px-8 mt-32'>
+        <div className='container mx-auto px-8 mt-35'>
           <Hero />
           <Technologies />
           <Education />
--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -8,7 +8,7 @@ const ExperienceCard = ({ time, company, desc, role, location, link }) => {
         viewport={{ once: true }}
         whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: -100 }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration: 0.8 }}
         className='w-full lg:w-1/4 pt-4 px-2'
       >
         <p className='mb-2 text-sm text-neutral-400'>{time}</p>
@@ -19,7 +19,7 @@ const ExperienceCard = ({ time, company, desc, role, location, link }) => {
         viewport={{ once: true }}
         whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: 100 }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration: 0.4 }}
         className='w-full max-w-xl lg:w-3/4 card py-4 px-2 rounded-xl group'
       >
         <a

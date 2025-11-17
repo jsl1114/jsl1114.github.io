6b09cfe2b99e6c9ff508c204abd34325186887ae
jsl1114 <ljs11141118@gmail.com>
2025-11-17 16:54:16 -0500

    feat: match system color scheme automatically

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -3,18 +3,29 @@ import { createContext, useState, useEffect } from 'react';
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
+  const matchedSysDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches
   const [theme, setTheme] = useState(() => {
-    const savedTheme = localStorage.getItem('theme');
-    return savedTheme || 'dark';
+    const savedTheme = localStorage.getItem("theme")
+    return matchedSysDarkMode ? "dark" : savedTheme || 'light';
   });
 
   useEffect(() => {
-    localStorage.setItem('theme', theme);
+    const mq = window.matchMedia("(prefers-color-scheme: dark)")
+
+    if (mq.matches) {
+      setTheme("dark")
+    }
+
+    mq.addEventListener('change', (e) => setTheme(e.matches ? "dark" : "light"))
+  }, [])
+
+  useEffect(() => {
     if (theme === 'dark') {
       document.documentElement.classList.add('dark');
     } else {
       document.documentElement.classList.remove('dark');
     }
+    localStorage.setItem('theme', theme);
   }, [theme]);
 
   const toggleTheme = () => {

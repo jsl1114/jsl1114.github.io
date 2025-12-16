0aceaca0d3760d797066e9e4fb8567afdc7919fd
jsl1114 <ljs11141118@gmail.com>
2025-12-16 16:04:38 -0500

    feat: dynamic display of year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
       <div className="text-sm font-medium text-neutral-600 dark:text-neutral-400 flex justify-center p-8">
-        <p className="mr-1 italic">Jason Liu 2025</p>
         <p>&copy;</p>
+        <p className="mr-1 italic">Jason Liu {new Date().getFullYear()}</p>
       </div>
   );
 };

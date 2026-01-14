d04009286a3f9880f8a80d71ff3771a6d5feb252
jsl1114 <ljs11141118@gmail.com>
2026-01-13 19:42:56 -0500

    force redeploy

--- server/index.jsx
+++ server/index.jsx
@@ -2,5 +2,5 @@ import app from "./app.js";
 import { config } from "./config/config.js";
 
 app.listen(config.port, () => {
-  console.log(`Email server running at ${config.serverEndPoint}`);
+  console.log(`Email server running at ${config.serverEndPoint}.`);
 });

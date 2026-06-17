568c5d78883f845cb0db6713d1ce0800ea96a0ea
jsl1114 <ljs11141118@gmail.com>
2026-06-17 14:51:14 -0400

    feat(design): load Inter + Libre Baskerville, serif headings / sans body

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "@fontsource-variable/inter";
+import "@fontsource/libre-baskerville/400.css";
+import "@fontsource/libre-baskerville/700.css";
 import ReactGA from "react-ga4";
 
 ReactGA.initialize("G-8MVEF01ZBJ");
